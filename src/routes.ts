49fca572582c3e41ea146be18115e0b5f2f30a60
IACarvalho <israelcarvalhodesenvolvedor@gmail.com>
2021-02-26 00:28:25 -0800

    Dia 04 NLW04

--- src/routes.ts
+++ src/routes.ts
@@ -1,15 +1,19 @@
 import { UserController } from "./controllers/UserController";
-import { Router } from "express";
 import { SurveysController } from "./controllers/SurveysController";
+import { SendMailCOntroller } from "./controllers/SendMailCOntroller";
+import { Router } from "express";
 
 const router = Router();
 
 const userController = new UserController();
 const surveysController = new SurveysController();
+const sendMailCOntroller = new SendMailCOntroller();
 
 router.post("/users", userController.create);
 
 router.post("/surveys", surveysController.create);
 router.get("/surveys", surveysController.show);
+
+router.post("/sendMail", sendMailCOntroller.execute);
 
 export { router };
